4803b422265072e856f9cba80f1fa65721a5ba9c
clxzz <37508905+clxzz@users.noreply.github.com>
2018-08-02 23:31:54 +0800

    Update largestRectangleArea.cpp

--- largestRectangleArea.cpp
+++ largestRectangleArea.cpp
@@ -13,13 +13,44 @@ int maximalRectangle(vector<vector<char>>& matrix);
     	vector<int> h={2,1,5,6,2,3};
     	int res=largestRectangleArea(h);
     	cout<<res<<endl;
-    	vector<vector<char> > matrix={{"1","0","1","0","0"},
-								   	  {"1","0","1","1","1"},
-								  	  {"1","1","1","1","1"},
-								  	  {"1","0","0","1","0"}};
-
+    	vector<vector<char> > matrix={{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','1','1','1'}};
+		int res1 = maximalRectangle(matrix);
+		cout<<res1<<endl;
     	return 0;
     }
+
+int maximalRectangle(vector<vector<char>>& matrix) 
+{
+	int m=matrix.size(), n=matrix[0].size();
+	int res=0;
+	vector<int> rowHeight(n,0);
+	for(int i=0;i<m;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(matrix[i][j] == '1')
+			{
+				rowHeight[j] += 1;
+			}
+			else
+			{
+				rowHeight[j] = 0;
+			}
+		}
+		int res_tmp=largestRectangleArea(rowHeight);
+		res = Max(res, res_tmp);
+	}
+	return res;
+}
+
+int Min(int a, int b)
+{
+	return a<b?a:b;
+}
+int Max(int a,int b)
+{
+	return a>b?a:b;
+}
 
 int largestRectangleArea(vector<int>& height)
 {
@@ -39,19 +70,4 @@ int largestRectangleArea(vector<int>& height)
             }
         }
         return res;
-}
-
-int maximalRectangle(vector<vector<char>>& matrix) 
-{
-	int m=matrix.size(), n=matrix[0].size();
-	        
-}
-
-int Min(int a, int b)
-{
-	return a<b?a:b;
-}
-int Max(int a,int b)
-{
-	return a>b?a:b;
 }
